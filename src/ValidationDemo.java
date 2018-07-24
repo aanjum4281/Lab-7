ac1daf263d5fdef146bff2ec06593d03e5f49fc3
Amna <aanjum4281@gmail.com>
2018-07-23 23:19:56 -0400

    added comments

--- src/ValidationDemo.java
+++ src/ValidationDemo.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class ValidationDemo {
 	
+	//Amna Anjum
+	
+	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		
+		//Name validation 
 		String name = Validator.getStringMatchingRegex(scnr, 
 				"Enter first name: ", "[A-Z][a-z]*");
 		
+		//email validation
 		String email = Validator.getStringMatchingRegex(scnr, 
 				"Enter your email ", "[A-Za-z0-9]{10}@[A-Za-z0-9]{5}.[A-Za-z]{2,3}");
 		
+		//phone number validation
 		String phone = Validator.getStringMatchingRegex(scnr, 
 				"Enter your phone number ", "[0-9]{3}-[0-9]{3}-[0-9]{4}");
 		
+		//birthday validation 
 		String bday = Validator.getStringMatchingRegex(scnr,
 				"Enter your birthday " , "^\\d{2}/\\d{2}/\\d{4}" );
 				
